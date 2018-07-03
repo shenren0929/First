19daa902193dfb73b8bf8de09e8b17e6bc34f692
shenren0929 <1509163326@qq.com>
2018-07-03 17:11:46 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" exclude="Detail">
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style></style>
